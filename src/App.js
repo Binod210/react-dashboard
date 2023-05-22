a05cdf9775842750a2978a71179f3615dfa27fc7
Binod Thapa <thapabinod089@gmail.com>
2023-05-22 09:59:01 +0530

    Changes on appbar and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Settings from "./pages/Setting";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" exact element={<About />} />
+          <Route path="/settings" exact element={<Settings />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
